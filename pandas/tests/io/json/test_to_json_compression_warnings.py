62871059a01144ced8dcddbba2a506b7d020b75b
root <rahmatullah.nikyar@gmail.com>
2020-03-11 18:28:48 +0000

    added another test case for no compression

--- pandas/tests/io/json/test_to_json_compression_warnings.py
+++ pandas/tests/io/json/test_to_json_compression_warnings.py
@@ -7,19 +7,29 @@ import warnings
 
 class TestToJsonWarnings:
 
-    def test_compression_with_paths(self):
+    # testing that to_json with a path and compression specified does not cause any warnings
+    def test_warning_compression_with_paths(self):
         with warnings.catch_warnings(record=True) as w:
             with tm.ensure_clean() as path:
                 df = pd.DataFrame({"a": [1, 2, 3, 4], "b": ["A", "B", "C", "D"],})
                 json_df = df.to_json(path, compression="gzip")
             assert len(w) == 0
     
-    def test_compression_without_path(self):
+    # test that to_json does generate the specified warning, when compression is specified and path is not
+    def test_warning_compression_without_path(self):
         with warnings.catch_warnings(record=True) as w: 
             df = pd.DataFrame({"a": [1, 2, 3, 4], "b": ["A", "B", "C", "D"],})
             json_df = df.to_json(compression="gzip")
             assert len(w) == 1
             assert issubclass(w[0].category, Warning)
             assert "without compression" in str(w[0].message)
+    
+    # test that to_json without compression but path specified generates no warning
+    def test_warning_path_without_compression(self):
+        with warnings.catch_warnings(record=True) as w: 
+            df = pd.DataFrame({"a": [1, 2, 3, 4], "b": ["A", "B", "C", "D"],})
+            with tm.ensure_clean() as path:
+                json_df = df.to_json(path)
+            assert len(w) == 0
 
 
